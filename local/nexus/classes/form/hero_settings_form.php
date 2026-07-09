209ecbd5d178aeb7508f679be48a4f434ddf7e77
Lewis Bedar <46265164+lewisbedar@users.noreply.github.com>
2026-07-09 21:29:51 +0200

    Fix recent courses sorting

---
+++ local/nexus/classes/form/hero_settings_form.php
@@ -0,0 +1,41 @@
+<?php
+namespace local_nexus\form;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->libdir . '/formslib.php');
+
+class hero_settings_form extends \moodleform {
+    public function definition(): void {
+        $mform = $this->_form;
+
+        $mform->addElement('text', 'hero_title', get_string('herotitle', 'local_nexus'));
+        $mform->setType('hero_title', PARAM_TEXT);
+        $mform->addRule('hero_title', null, 'required');
+
+        $mform->addElement('textarea', 'hero_subtitle', get_string('herosubtitle', 'local_nexus'), ['rows' => 3, 'cols' => 60]);
+        $mform->setType('hero_subtitle', PARAM_TEXT);
+        $mform->addRule('hero_subtitle', null, 'required');
+
+        $mform->addElement('text', 'hero_primary_label', get_string('heroprimarylabel', 'local_nexus'));
+        $mform->setType('hero_primary_label', PARAM_TEXT);
+        $mform->addRule('hero_primary_label', null, 'required');
+
+        $mform->addElement('text', 'hero_primary_url', get_string('heroprimaryurl', 'local_nexus'));
+        $mform->setType('hero_primary_url', PARAM_URL);
+        $mform->addRule('hero_primary_url', null, 'required');
+
+        $mform->addElement('text', 'hero_secondary_label', get_string('herosecondarylabel', 'local_nexus'));
+        $mform->setType('hero_secondary_label', PARAM_TEXT);
+        $mform->addRule('hero_secondary_label', null, 'required');
+
+        $mform->addElement('text', 'hero_secondary_url', get_string('herosecondaryurl', 'local_nexus'));
+        $mform->setType('hero_secondary_url', PARAM_URL);
+        $mform->addRule('hero_secondary_url', null, 'required');
+
+        $mform->addElement('advcheckbox', 'nexus_as_home', get_string('nexusashome', 'local_nexus'));
+        $mform->setDefault('nexus_as_home', 0);
+
+        $this->add_action_buttons(false, get_string('savechanges'));
+    }
+}
--- local/nexus/classes/manager.php
+++ local/nexus/classes/manager.php
@@ -3,6 +3,7 @@ namespace local_nexus;
 
 defined('MOODLE_INTERNAL') || die();
 
+use local_nexus\local\application_service;
 use local_nexus\output\application;
 
 class manager {
@@ -11,7 +12,7 @@ class manager {
 
         $records = $DB->get_records(
             'local_nexus_applications',
-            ['enabled' => 1],
+            ['enabled' => 1, 'showdock' => 1],
             'sortorder ASC, name ASC'
         );
 
@@ -20,10 +21,14 @@ class manager {
         foreach ($records as $record) {
             $applications[] = new application(
                 $record->name,
-                $record->icon ?? '',
+                application_service::get_icon_url($record),
                 $record->url,
                 $record->description ?? '',
-                $record->visibility !== 'public'
+                $record->visibility !== 'public',
+                $record->version ?? '',
+                $record->status ?? 'stable',
+                $record->category ?? '',
+                $record->color ?? ''
             );
         }
 
